f5c24943f2748361cadabe4159bbf259746c93a5
david <18811409796@163.com>
2016-12-29 19:44:51 +0800

    Create the structure.

---
+++ app/src/main/java/cn/edu/buaa/david/myviewpager/CustomizedParameters.java
@@ -0,0 +1,10 @@
+package cn.edu.buaa.david.myviewpager;
+
+/**
+ * Created by david on 2016/12/29.
+ */
+
+public class CustomizedParameters {
+    protected int MODE_STATIC = 0, MODE_AUTO = 1;
+    protected int TIME_INTERVEL = 5;
+}
---
+++ app/src/main/java/cn/edu/buaa/david/myviewpager/MyViewPager.java
@@ -0,0 +1,19 @@
+package cn.edu.buaa.david.myviewpager;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * Created by david on 2016/12/29.
+ */
+
+public class MyViewPager extends ViewPager {
+    public MyViewPager(Context context) {
+        super(context);
+    }
+
+    public MyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
